d8298ba7ff6f0feea0ad25f83363f2b136a19d6a
hyamauchi <hitomi.yamauchi@d2cr.co.jp>
2020-06-23 14:39:45 +0900

    Add:ひとまず保存

---
+++ vendingmachine.py
@@ -0,0 +1,128 @@
+#実習課題2
+#vending_machine
+
+# coding:utf-8
+
+#Flask,テンプレート,リクエスト読み込み
+from flask import Flask, render_template, request
+
+import mysql.connector
+from mysql.connector import errorcode
+
+#時間の取得
+import datetime
+#更新日時の取得
+import os
+
+#画像処理
+from PIL import Image
+
+#自分をappという名称でインスタンス化
+app = Flask(__name__)
+
+ #データベースの情報
+host = 'localhost' # データベースのホスト名又はIPアドレス
+username = 'root'  # MySQLのユーザ名
+passwd   = 'Hito05hito'    # MySQLのパスワード
+dbname   = 'my_database'    # データベース名
+
+#商品管理ページ（初期画面、常にテーブルが表示されるように）
+@app.route('/management')
+def management_send():
+
+    drink = []
+    try:
+        cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
+        cursor = cnx.cursor()
+        query = 'SELECT drink.image, drink.name, drink.price, stock.stock drink.status FROM drink LEFT JOIN stock ON drink.drink_id = stock.drink_id'
+        cursor.execute(query)
+
+        for (id, image, name, price, stock, status) in cursor:
+            item = {"drink_id":id, "image":image, "name":name, "price":price, "stock":stock,"status":status}
+            drink.append(item)
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("ユーザ名かパスワードに問題があります。")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("データベースが存在しません。")
+        else:
+            print(err)
+    else:
+        cnx.close()
+    return render_template('management.html', drink=drink)
+
+#商品管理ページ（追加・更新など、変更後の画面）
+@app.route('/management',methods=['POST'])
+def management_recieve():
+
+    #初期値
+    id = request.form.get("drink_id","")
+    name = request.form.get("new_name","")
+    price = request.form.get("new_price","")
+    stock = request.form.get("new_stock","")
+    image = request.form.get("new_image","")
+    status = request.form.get("new_status","")
+
+    #日時の取得
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    #入力の判定（エラー文を表示する）
+    result = ""
+
+    cnx = None
+
+    try:
+        cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
+        cursor = cnx.cursor()
+
+        #常にテーブルは表示
+        query = 'SELECT drink.image, drink.name, drink.price, stock.stock drink.status FROM drink LEFT JOIN stock ON drink.drink_id = stock.drink_id'
+
+        if image == "" or name == "" or price == "" or stock == "" or status == "":
+            cursor.execute(query)
+            result = "全ての項目を入力してください。"  
+        
+        else:
+            #テーブルに追加する
+            try:
+                sql = "INSERT INTO drink (drink_id, name, image, price, created_date, update_date, status) VALUES({}, {}, {},{},{},{},{})".format(id, name, image, price, date, date, status)
+                cursor.execute(sql)
+                #order_id = cursor.lastrowid # insertした値を取得できます。
+                
+                sql = "INSERT INTO stock (drink_id, stock, created_date, update_date) VALUES({}, {}, {},{})".format(id, stock, date, date)
+                cursor.execute(sql)
+
+                cnx.commit()
+
+            except mysql.connector.Error as err:
+                print(err)
+                result = "「JPEG」または「PNG」形式の画像をアップロードしてください。"
+
+            cursor.execute(query)
+
+        drink = []
+        for (id, image, name, price, stock, status) in cursor:
+            item = {"drink_id":id, "image":image, "name":name, "price":price, "stock":stock, "status":status}
+            drink.append(item)
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("ユーザ名かパスワードに問題があります。")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("データベースが存在しません。")
+        else:
+            print(err)
+    else:
+        cnx.close()
+    return render_template('management.html', drink=drink, result=result)
+
+#購入画面
+#@app.route('/purchase',methods=['GET','POST'])
+#def purchase():
+#    return render_template('purchase.html')
+
+#購入結果画面
+#@app.route('/result',methods=['POST'])
+#def result():
+#    return render_template('result.html')
