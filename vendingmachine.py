f0e2d70c826fc66c3fc30735221d66c4916bdcdb
hyamauchi <hitomi.yamauchi@d2cr.co.jp>
2020-06-25 11:46:29 +0900

    fix

--- vendingmachine.py
+++ vendingmachine.py
@@ -31,22 +31,28 @@ username = 'root'  # MySQLのユーザ名
 passwd   = 'Hito05hito'    # MySQLのパスワード
 dbname   = 'my_database'    # データベース名
 
-#画像ファイルのpathを指定
-UPLOAD_FOLDER = '/Users/hyamauchi/Desktop/codecamp_work/vendingmachine/drink_image/'
-ALLOWED_EXTENSIONS = {'png','jpeg'}
+#アップロード画像の保存場所
+#UPLOAD_FOLDER = '/Users/hyamauchi/Desktop/codecamp_work/vendingmachine/drink_image'
+UPLOAD_FOLDER = './drink_image/'
+ALLOWED_EXTENSIONS = set(['png','jpeg'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#商品管理ページ（初期画面）
+#def allowed_file(filename):
+#    return '.' in filename and \
+#        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+#管理者画面（ホーム画面）
 @app.route('/management', methods=['GET'])
 def management():
 
-    drink = []
     try:
         cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
         cursor = cnx.cursor()
+
         query = 'SELECT drink.image, drink.name, drink.price, stock.stock, drink.status FROM drink LEFT JOIN stock ON drink.drink_id = stock.drink_id;'
         cursor.execute(query)
 
+        drink = []
         for (image, name, price, stock, status) in cursor:
             item = {"image":image, "name":name, "price":price, "stock":stock, "status":status}
             drink.append(item)
@@ -62,7 +68,8 @@ def management():
         cnx.close()
     return render_template('management.html', drink=drink)
 
-#商品管理ページ（追加・更新など、変更後）
+
+#管理者画面（追加・更新）
 @app.route('/management',methods=['POST'])
 def management_recieve():
 
@@ -75,24 +82,29 @@ def management_recieve():
 
     #画像の取得
     #アップロードされていない場合はNone
-    image = request.files.get("new_img","")
-
-    # secure_filenameを使ってLinuxでも保存可能なファイル名に変換
+    image = request.files["new_img"]
+    #画像の保存
     filename = secure_filename(image.filename)
-    image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'],filename)
+    
+    #公開・非公開ステータス
+    status_public = 1
+    status_private = 0
+
+    #完了メッセージ
+    success_message = ""
+
+    #エラーメッセージ
+    error_message = ""
+    error_message_price = ""
+    error_message_stock = ""
+    error_message_image = ""
 
     #sqlの状態
     #insert:追加、update:在庫の更新、change:公開・非公開の変更
     sql_kind = request.form.get("sql_kind","")
 
-    #完了メッセージ
-    #success_message = ""
-
-    #エラーメッセージ
-    error_message = ""
-    #error_message_price = ""
-    #error_message_stock = ""
-    #error_message_image = ""
+    #tryの前に入力項目の確認
 
     try:
         cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
@@ -103,31 +115,53 @@ def management_recieve():
 
         if image == None or name == "" or price == "" or stock == "" or status == "":
             cursor.execute(query)
-            error_message = "全ての項目を入力してください。" 
+            error_message = "いずれかの項目が未入力です。全ての項目を入力してください。" 
+
+            #価格が0以上か確認
+            if re.match('^[0-9]$', price):
+                error_message_price = ""
+            else:
+                error_message_price = "価格は0以上の整数で入力してください。"
+    
+            #在庫数が0以上か確認
+            if re.match('^[0-9]$', price):
+                error_message_stock = ""
+            else:
+                error_message_stock = "在庫数は0以上の整数で入力してください。"
+            
+            #画像の形式を確認
+            #if image and allowed_file(image.filename):
+            #filename = secure_filename(image.filename)
+            #image.save(os.path.join(app.config['UPLOAD_FOLDER'],filename)
+            #img_url = '/drink_image/' + filename
+            #error_message_image = " "
+
+            #else:
+            #    error_message_image = '許可されていないファイル形式です。'
         
         else:
             #sql_kindがinsertの時
             #sql_kindがupdateの時
-            #sql_kindがの時
+            #sql_kindがchangeの時
             # if文
             # priceの話
             # stockの話
-            # file読み込み
+
             try:
-                sql_drink = "INSERT INTO drink (name, image, price, status) VALUES('{}', '{}', {}, {})".format(name, image, price, status)
-                cursor.execute(sql_drink)
-                drink_id = cursor.lastrowid # insertした値を取得できます。
-                
-                sql_stock = "INSERT INTO stock (drink_id, stock) VALUES({}, {})".format(drink_id, stock)
-                cursor.execute(sql_stock)
+                add_drink = "INSERT INTO drink (name, image, price, status) VALUES('{}', '{}', {}, {})".format(name, image, price, status)
+                #drink_id = cursor.lastrowid # insertした値を取得できます。 
+                add_stock = "INSERT INTO stock (drink_id, stock) VALUES({}, {})".format(drink_id, stock)
+                cursor.execute(add_drink)
+                cursor.execute(add_stock)
                 cnx.commit()
+                success_message = "【追加成功】商品が正しく追加されました"
 
             except mysql.connector.Error as err:
                 print(err)
 
-            cursor.execute(query)
 
-            #sql_kind == 'insert':
+            #必ず実行
+            cursor.execute(query)
 
         drink = []
         for (image, name, price, stock, status) in cursor:
@@ -135,8 +169,8 @@ def management_recieve():
             drink.append(item)
 
         params = {
-            "drink" : drink
-            #message
+            "drink" : drink,
+            "success_message" : success_message
 
         }
 
