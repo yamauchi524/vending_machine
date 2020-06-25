b46b78340317bd8d4a1ee1b5937251adf8c16bdf
hyamauchi <hitomi.yamauchi@d2cr.co.jp>
2020-06-25 21:50:14 +0900

    fix

--- vendingmachine.py
+++ vendingmachine.py
@@ -34,16 +34,12 @@ dbname   = 'my_database'    # データベース名
 #アップロード画像の保存場所
 #UPLOAD_FOLDER = '/Users/hyamauchi/Desktop/codecamp_work/vendingmachine/drink_image'
 UPLOAD_FOLDER = './drink_image/'
-ALLOWED_EXTENSIONS = set(['png','jpeg'])
+#ALLOWED_EXTENSIONS = set(['png','jpeg'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-#def allowed_file(filename):
-#    return '.' in filename and \
-#        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 #管理者画面（ホーム画面）
 @app.route('/management', methods=['GET'])
-def management():
+def management_home():
 
     try:
         cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
@@ -53,8 +49,8 @@ def management():
         cursor.execute(query)
 
         drink = []
-        for (id, image, name, price, stock, status) in cursor:
-            item = {"drink_id":id, "image":image, "name":name, "price":price, "stock":stock, "status":status}
+        for (drink_id, image, name, price, stock, status) in cursor:
+            item = {"drink_id":drink_id, "image":image, "name":name, "price":price, "stock":stock, "status":status}
             drink.append(item)
 
     except mysql.connector.Error as err:
@@ -68,10 +64,9 @@ def management():
         cnx.close()
     return render_template('management.html', drink=drink)
 
-
 #管理者画面（追加・更新）
 @app.route('/management',methods=['POST'])
-def management_recieve():
+def management_drink():
 
     #変数の定義
     drink_id = request.form.get("drink_id","")
@@ -86,11 +81,12 @@ def management_recieve():
     #画像の保存
     filename = secure_filename(image.filename)
     image.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    #パスを含めて保存
+    #sql_image = '/drink_image/' + filename
     
-    #公開・非公開ステータス
-    #status_public = 1
-    #status_private = 0
-
+    #変更後の公開ステータス
+    next_status = request.form.get("change_status","")
+    
     #完了メッセージ
     success_message = ""
 
@@ -98,45 +94,42 @@ def management_recieve():
     error_message = ""
     error_message_price = ""
     error_message_stock = ""
-    error_message_image = ""
+    #error_message_image = ""
 
     #sqlの状態
-    #insert:追加、update:在庫の更新、change:公開・非公開の変更
+    #insert:追加、update:在庫数の更新、change:公開・非公開ステータスの変更
     sql_kind = request.form.get("sql_kind","")
 
     try:
+        #DBの読み込み
         cnx = mysql.connector.connect(host=host, user=username, password=passwd, database=dbname)
         cursor = cnx.cursor()
 
         #常にテーブルは表示
         query = 'SELECT drink.drink_id, drink.image, drink.name, drink.price, stock.stock, drink.status FROM drink LEFT JOIN stock ON drink.drink_id = stock.drink_id;'
 
-        if image == None or name == "" or price == "" or stock == "" or status == "":
-            cursor.execute(query)
-            error_message = "いずれかの項目が未入力です。全ての項目を入力してください。" 
-        
-        else:
-            #sql_kindがinsertの時
-            #sql_kindがupdateの時
-            #sql_kindがchangeの時
+        #商品の追加
+        if sql_kind == 'insert':
 
-            #追加
-            if sql_kind == 'insert':
+            #いずれかの項目が入力されていない場合
+            if image == None or name == "" or price == "" or stock == "":
+                cursor.execute(query)
+                error_message = "いずれかの項目が未入力です。全ての項目を入力してください。" 
 
+            else:
                 try:
-                    add_drink = "INSERT INTO drink (name, image, price, status) VALUES('{}', '{}', {}, {})".format(name, image, price, status)
-                    #drink_id = cursor.lastrowid # insertした値を取得できます。 
+                    add_drink = "INSERT INTO drink (name, image, price, status) VALUES ('{}', '{}', {}, {})".format(name, image, price, status)
+                    drink_id = cursor.lastrowid # insertした値を取得できます。 
+
                     add_stock = "INSERT INTO stock (drink_id, stock) VALUES({}, {})".format(drink_id, stock)
                     cursor.execute(add_drink)
                     cursor.execute(add_stock)
                     cnx.commit()
 
-                    success_message = "【追加成功】商品が正しく追加されました"
+                    success_message = "【追加成功】商品が追加されました"
 
                 except mysql.connector.Error as err:
                     print(err)
-
-                    error_message_image = "許可されていないファイル形式です。"
 
                     #価格が0以上か確認
                     if re.match('^[0-9]$', price):
@@ -145,55 +138,76 @@ def management_recieve():
                         error_message_price = "価格は0以上の整数で入力してください。"
     
                     #在庫数が0以上か確認
-                    if re.match('^[0-9]$', price):
+                    if re.match('^[0-9]$', stock):
                         error_message_stock = ""
                     else:
                         error_message_stock = "在庫数は0以上の整数で入力してください。"
                     
                 #必ず実行
                 cursor.execute(query)
-            
-            #在庫数の更新
-            elif sql_kind == 'update':
-                try:
-                    update_drink = "UPDATE drink (name, image, price, status) VALUES('{}', '{}', {}, {})".format(name, image, price, status)
-                    #drink_id = cursor.lastrowid # insertした値を取得できます。 
-                    update_stock = "INSERT INTO stock (drink_id, stock) VALUES({}, {})".format(drink_id, stock)
-                    cursor.execute(update_drink)
-                    cursor.execute(update_stock)
-                    cnx.commit()
 
-                    success_message = "【更新成功】在庫数が正しく更新されました"
+        #在庫数の更新
+        elif sql_kind == 'update':
 
-                except mysql.connector.Error as err:
-                    print(err)
+            try:
+                update_stock = "UPDATE stock SET stock = {} WHERE drink_id = {}".format(stock, drink_id)
+                #drink_id = cursor.lastrowid # insertした値を取得できます。 
 
-                    #在庫数が0以上か確認
-                    if re.match('^[0-9]$', price):
-                        error_message_stock = ""
-                    else:
-                        error_message_stock = "在庫数は0以上の整数で入力してください。"
+                #add_stock = "INSERT INTO stock (drink_id, stock) VALUES({}, {})".format(drink_id, stock)
+                #cursor.execute(add_drink)
+                cursor.execute(update_stock)
+                cnx.commit()
 
-                #必ず実行
-                cursor.execute(query)
+                success_message = "【更新成功】在庫数が変更されました。"
 
-            else:    
-                #必ず実行
-                cursor.execute(query)
+            except mysql.connector.Error as err:
+                print(err)
+    
+                #在庫数が0以上か確認
+                if re.match('^[0-9]$', stock):
+                    error_message_stock = ""
+                else:
+                    error_message_stock = "在庫数は0以上の整数で入力してください。"
+                    
+            #必ず実行
+            cursor.execute(query)
 
+        #公開・非公開ステータスの変更
+        elif sql_kind == 'change':
+            try:
+                #公開→非公開へ
+                if status == 1:
+                    change_status = "UPDATE drink SET status = {} WHERE drink_id = {}".format(next_status, drink_id)
+
+                #非公開・公開へ
+                else:
+                    change_status = "UPDATE drink SET status = {} WHERE drink_id = {}".format(next_status, drink_id)
+
+                cursor.execute(change_status)
+                cnx.commit()
+
+                success_message = "【更新成功】公開ステータスが変更されました。"
+
+            except mysql.connector.Error as err:
+                print(err)
+                    
+            #必ず実行
+            cursor.execute(query)
+
+        else:
             #必ず実行
             cursor.execute(query)
 
         drink = []
-        for (id, image, name, price, stock, status) in cursor:
-            item = {"drink_id":id, "image":image, "name":name, "price":price, "stock":stock, "status":status}
+        for (drink_id, image, name, price, stock, status) in cursor:
+            item = {"drink_id":drink_id, "image":image, "name":name, "price":price, "stock":stock, "status":status}
             drink.append(item)
 
         params = {
             "drink" : drink,
             "success_message" : success_message,
             "error_message": error_message,
-            "error_message_image":error_message_image,
+            #"error_message_image":error_message_image,
             "error_message_price":error_message_price,
             "error_message_stock":error_message_stock
         }
