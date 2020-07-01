b1e76728f11e6a2d22dbd61b7deced99beb9cb6a
hyamauchi <hitomi.yamauchi@d2cr.co.jp>
2020-07-01 16:30:24 +0900

    store the same drink_id

--- purchase_table.sql
+++ purchase_table.sql
@@ -1,5 +1,6 @@
 create table purchase(
     drink_id INT,
-    purchase_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(drink_id)
+    purchase_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+/*PRIMARY KEY(drink_id)
